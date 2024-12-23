754ba20d69bc113ab0913bc926eb30b911f92217
Alexander Fagerdal <alexanderfagerdal01@gmail.com>
2024-12-23 10:19:14 +0100

    Ready for deployment on Vercel

--- public/src/services/apiService.ts
+++ public/src/services/apiService.ts
@@ -3,7 +3,7 @@ import { get, post, put, del } from "./serviceBase";
 import { handleApiError } from "../utils/apiHelpers";
 import { Movie, MovieResponse } from "../interface/movie";
 
-const API_BASE_URL = "http://localhost:8000/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 interface IUser {
   id: number;
